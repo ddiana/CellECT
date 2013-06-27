4e832b197fead0325cadf0846547869ce9e43973
diana <diana@diana-lab.(none)>
2013-06-26 17:37:00 -0700

    added exception handling in load_parameters

--- CellECT/seg_tool/globals.py
+++ CellECT/seg_tool/globals.py
@@ -13,3 +13,27 @@ _std_per_dim = []
 should_load_last_save = False
 
 task_index = 0
+
+default_parameter_dictionary_keys = ("volume_mat_path",
+   "volume_mat_var",\
+   "first_seg_mat_path",\
+   "first_seg_mat_var", \
+   "nuclei_mat_path",\
+   "nuclei_mat_var",\
+   "training_vol_mat_path",\
+   "training_vol_mat_var",\
+   "training_vol_nuclei_mat_path", \
+   "training_vol_nuclei_mat_var",\
+   "training_positive_seg_mat_path",\
+   "training_positive_seg_mat_var", \
+   "training_positive_labels_mat_path",\
+   "training_positive_labels_mat_var",\
+   "training_negative_seg_mat_path",\
+   "training_negative_seg_mat_var",\
+   "training_negative_labels_mat_path",\
+   "training_negative_labels_mat_var", \
+   "save_location_prefix",\
+   "has_bg", \
+   "use_size", \
+   "use_border_intensity", \
+   "use_border_distance")
--- CellECT/seg_tool/seg_io/load_parameters.py
+++ CellECT/seg_tool/seg_io/load_parameters.py
@@ -4,25 +4,95 @@
 # Imports
 import pdb
 import re
+import sys
+from termcolor import colored
 
 # Imports from this project
 import CellECT.seg_tool.globals 
+
+
+def parse_config_file_line(line, line_number):
+	"""
+	Parse a line from the config fine: key = value.
+	"""
+
+	if not line.strip():
+		return None, None
+	
+	matches = re.match("(.+)=(.+)", line)
+
+	found_issue = False
+
+	if matches:
+		key = matches.group(1).strip().lower()
+		val = matches.group(2).strip().lower()
+
+		if not key in CellECT.seg_tool.globals.default_parameter_dictionary_keys:
+			found_issue = True
+
+		if not val:
+			found_issue = True
+	else:
+		found_issue = True
+
+	if found_issue:
+		print colored("Error reading config file at line " + str(line_number)+": "+line, 'red')
+		raise IOError("Error reading config file at line " + str(line_counter)+": "+line)
+
+	if CellECT.seg_tool.globals.DEFAULT_PARAMETER.has_key(key):
+		print colored("Error reading config file at line " + str(line_number) + ": Redefinition of key '" + key+"'", "red")
+		raise IOError("Error reading config file at line " + str(line_number) + ": Redefinition of key '" + key+"'")
+			
+	return key, val
+
+
+def check_if_file_complete():
+	"""
+	Check if the config file addressed all the parameters needed.
+	"""
+
+	missing_info = ""
+	for key in CellECT.seg_tool.globals.default_parameter_dictionary_keys:
+		if not CellECT.seg_tool.globals.DEFAULT_PARAMETER.has_key(key):
+			missing_info += key + " "
+
+	if len(missing_info):
+		print colored("Error reading config file: incomplete config file. \nMissing info: "+missing_info, "red")
+		raise IOError("Error reading config file: incomplete config file. Minssing info: "+missing_info)
+
 
 
 def read_program_parameters(config_file_path):
 
 	"Read parameters from config file."
 
-	f = open(config_file_path)
+	try:
+		f = open(config_file_path)
+	except IOError as err:
+		print colored("Could not read config file at:" + config_file_path, 'red')
+		sys.exit()
+
 	line = f.readline()
-	
+	line_counter = 0
+
 	while (line != ""):
-		matches = re.match("(.*)=(.*)", line)
-		if matches:
-			key = matches.group(1).strip()
-			val = matches.group(2).strip()
+		line_counter += 1
+		try:
+			key, val = parse_config_file_line(line, line_counter)
+		except IOError as err:
+			sys.exit()
+			# TODO: write to log file
+		except Exception as err:
+			sys.exit()
+			# TODO: write to log file
+			
+		if key and val:
 			CellECT.seg_tool.globals.DEFAULT_PARAMETER[key] = val
-		else:
-			print "ERROR reading config file"
 		line = f.readline()
 
+	try:
+		check_if_file_complete()
+	except:
+		sys.exit()
+		# TODO: write to log file
+	
