4529d72406091866d8661fd67e6a974b72f42b4a
diana <diana@diana-lab.(none)>
2014-03-13 18:53:07 -0700

    making ui for merge predict

--- CellECT/seg_tool/cellness_metric/merge_predictor.py
+++ CellECT/seg_tool/cellness_metric/merge_predictor.py
@@ -1,18 +1,27 @@
 import heapq
 
 
+from CellECT.seg_tool.features.segment_features import get_bounding_box_union_two_segments
+from CellECT.seg_tool.gui.predict_merge_gui import MergePredictorUI
+
+
 class MergePredictor(object):
 
-	def __init__(self, seg_collection):
+	def __init__(self, seg_collection, vol, label_map):
 
 		self.build_heap(seg_collection)
+		self.seg_collection = seg_collection
+		self.label_map = label_map
+		self.ui = None
+		self.user_feedback = None
+		self.color_map = None
 
 
-	def build_heap(self, seg_collection):
+	def build_heap(self):
 
 		self.to_merge_queue = []
 
-		for segment in seg_collection.list_of_segments:
+		for segment in self.seg_collection.list_of_segments:
 			self.to_merge_queue.extend(self.get_scores_with_neighbors(segment))
 
 		heapq.heapify(self.to_merge_queue)
@@ -31,5 +40,27 @@ class MergePredictor(object):
 
 	def next_merge(self):
 		
-		return heapq.heappop(self.to_merge_queue)
+		merge_tuple =  heapq.heappop(self.to_merge_queue)
+
+		idx1 = self.seg_collection.segment_label_to_list_index_dict[merge_tuple[1]]
+		idx2 = self.seg_collection.segment_label_to_list_index_dict[merge_tuple[2]]
+		
+		seg1 = self.seg_collection.list_of_segments[idx1]
+		seg2 = self.seg_collection.list_of_segments[ids2]
+
+		bbx = get_bounding_box_union_two_segments(seg1.bounding_box, seg2.bounding_box)
+
+		vol = self.vol[bbx.xmin:bbx.xmax,  bbx.ymin:bbx.ymax,  bbx.zmin:bbx.zmax]
+		label_map = self.vol[bbx.xmin:bbx.xmax,  bbx.ymin:bbx.ymax,  bbx.zmin:bbx.zmax]
+		highlight_map = (label_map == 0) + 100*(label_map == seg1.label) + 200*(label_map == seg.label2)
+
+		self.ui = MergePredictorUI()
+		self.ui.set_data(vol, label_map, highlight_map)
+		self.ui.display()
+		self.user_feedback = self.ui.user_feedback
+		
+		
+
+
+
 
--- CellECT/seg_tool/gui/predict_merge_gui.py
+++ CellECT/seg_tool/gui/predict_merge_gui.py
@@ -9,7 +9,7 @@ class MergePredictorUI(object):
 		self.label_map = None
 		self.highlight_map = None
 
-		self.answer = None
+		self.answer = False
 
 
 	def set_data(self, vol, label_map, highlight_map)
@@ -19,10 +19,43 @@ class MergePredictorUI(object):
 		self.highlight_map = highlight_map
 
 
+	def merge_callback(self,event):
+		self.answer = True
+
+	def dont_merge_callback(self, event):
+		self.answer = False
+
+
 	def display(self):
 
 		self.fig = pylab.figure()
 
+		a_merge = pylab.axes([0.10, 0.05, 0.08, 0.05])
+		a_dont_merge= pylab.axes([0.20, 0.05, 0.16, 0.05])
+
+	
+		b_merge = Button(a_merge, 'Yes, merge.')
+		b_merge.on_clicked(self.merge_callback)
+		b_dont_merge = Button(a_dont_merge, "Don't merge / Don't know")
+		b_dont_merge.on_clicked(self.dont_merge_callback)
+
+		z0 = vol.shape[2] /2
+
+		ax1 = pylab.subplot(131)
+		pylab.subplots_adjust(bottom=0.25)
+		min_var_cmap_vol = self.vol.min()
+		max_var_cmap_vol = self..max()
+		l2 =  pylab.imshow(vol[:,:,z0], interpolation="nearest", cmap = "PRGn", vmin= min_var_cmap_vol, vmax = max_var_cmap_vol)   #cax = l2
+		pylab.axis()#[0, vol2.shape[0], 0, vol2.shape[1]])
+		ax2.set_title("Confocal Volume")
+
+		ax1 = pylab.subplot(132)
+		pylab.subplots_adjust(bottom=0.25)
+		min_var_cmap_vol = self.vol.min()
+		max_var_cmap_vol = self..max()
+		l2 =  pylab.imshow(vol[:,:,z0], interpolation="nearest", cmap = "PRGn", vmin= min_var_cmap_vol, vmax = max_var_cmap_vol)   #cax = l2
+		pylab.axis()#[0, vol2.shape[0], 0, vol2.shape[1]])
+		ax2.set_title("Confocal Volume")	
 		
 
 
